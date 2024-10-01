e3e1f7fa9d2efb3d72ced8478973a44d61598edb
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-01 02:26:31 +0000

    task 2

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,9 @@
+export default function taskBlock(trueorFalse) {
+  let task = false;
+  let task2 = true;
+  if (trueorFalse) {
+    let task = true;
+    let task2 = false;
+  }
+  return [task, task2];
+}
