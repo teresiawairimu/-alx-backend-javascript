4310cfed8c678a103c42f8b4b9ab71689287369d
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-03 17:32:30 +0000

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} ${this._code}`;
+  }
+}
