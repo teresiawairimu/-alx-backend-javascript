09c5b0df02870a06c9972519cf024238879d53af
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-04 13:55:02 +0000

    task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,6 +2,6 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFraciscoNeighborhoods;
+    return this.sanFranciscoNeighborhoods;
   };
 }
