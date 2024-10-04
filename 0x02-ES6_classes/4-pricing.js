ce9e75d1a751d2d12c2a6b2ca7c81c2311f7249e
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-04 12:14:38 +0000

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,7 +3,7 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
-    this.currency = new Currency(currency.code, currency.name);
+    this.currency = currency;
   }
 
   set amount(value) {
