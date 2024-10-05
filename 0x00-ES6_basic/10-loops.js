5ea318e580fe9a7ce232483ef77d7d07f33aec5d
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-05 00:20:55 +0000

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
+  for (let value of array) {
+    array[index] == appendString + value;
+  }
+  return array;
+}
